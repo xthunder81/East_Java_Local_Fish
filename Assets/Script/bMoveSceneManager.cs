66f66e87f279dbba2bfe6293e60e43818580eec0
x81 <x81>
2020-06-25 10:13:05 +0700

    add Linear Timer Game Object

---
+++ Assets/Script/bMoveSceneManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class bMoveSceneManager : MonoBehaviour
+{
+    //public string tampilan;
+
+    public void nextScene (string tampilan) {
+        SceneManager.LoadScene(tampilan);
+    }
+    
+    public void prevScene(string tampilan) {
+        SceneManager.LoadScene(tampilan);
+    }
+
+    
+}
