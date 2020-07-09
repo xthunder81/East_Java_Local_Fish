298cc1f4cd5f4298b406ababc3ca8ad7f8737790
x81 <xthunder81@rocketmail.com>
2020-07-09 19:35:41 +0700

    add fish material

---
+++ Assets/Script/spawnController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnController : MonoBehaviour
+{
+    public GameObject enemy;
+    public GameObject player;
+    public GameObject target;
+
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        target = GameObject.FindGameObjectWithTag("Target");
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
